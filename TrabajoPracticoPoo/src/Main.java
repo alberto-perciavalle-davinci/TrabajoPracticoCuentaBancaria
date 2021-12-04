c3589919b8cade44a74f7a4c06be7f5b6fea6b0a
Alberto Kevin Perciavalle <alberto.perciavalle@davinci.edu.ar>
2021-12-04 12:52:34 -0300

    Trabajo Practico Cuenta Bancaria - Grupo 1

---
+++ TrabajoPracticoPoo/src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+
+        CuentaBancaria primerCuenta = new CuentaBancaria("carlos", "35113454");
+
+        primerCuenta.mostrarMenu();
+
+
+    }
+}
